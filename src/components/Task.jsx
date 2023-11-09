b1df9e643e65e745253e93b15dd23c097fd9a6a2
vipin-100 <vipin.sharma@ck12.org>
2023-11-09 15:58:44 +0530

    changed the pin icon

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
-import PropTypes from "prop-types";
+
+import PropTypes from 'prop-types';
 export default function Task({
   task: { id, title, state },
   onArchiveTask,
@@ -50,7 +51,7 @@ export default function Task({
           aria-label={state === "TASK_PINNED" ? "unpin" : "pin"}
           key={`pinTask-${id}`}
         >
-          <span className={`icon-bell`} />
+         <span className={`icon-star`} />
         </button>
       )}
     </div>
